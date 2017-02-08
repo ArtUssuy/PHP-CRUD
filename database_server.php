78b3725efc2c46d539875ba76dffdd0046630546
artussuy <artussuy7@gmail.com>
2017-02-07 22:53:30 -0200

    Revert "CRUD completo"
    This reverts commit 48ba435c9921b20c3cba08fe050d1504bc6cc45c.

--- database_server.php
+++ database_server.php
@@ -1,13 +1,9 @@
 <?php
 
-	function DBExecute($query, $insertID = false){
+	function DBExecute($query){
 
 		$link = DBConnect();
 		$result = mysqli_query($link, $query) or die(mysqli_error($link));
-
-		if($insertID){
-			$result = mysqli_insert_id($link);
-		}
 		DBClose($link);
 
 		return $result;
@@ -15,21 +11,21 @@
 	}
 
 
-	function DBCreate($table, array $data, $insertID = false){
+	function DBCreate($table, array $data){
 
 		$fields = implode(', ', array_keys($data));
 		$values = "'".implode("', '", $data)."'";
 
 		$query = "INSERT INTO {$table} ({$fields}) VALUES ({$values})";
 
-		return DBExecute($query, $insertID);
+		return DBExecute($query);
 
 	}
 
 
-	function DBRead($table, $params = null, $fields = '*'){
+	function DBRead($table, $params = null, $fields = "*"){
 
-		$query = "SELECT {$fields} FROM {$table}{$params}";
+		$query = "SELECT * FROM {$table}";
 		$result = DBExecute($query);
 
 		if(!mysqli_num_rows($result))
@@ -39,34 +35,21 @@
 				while($res = mysqli_fetch_assoc($result))
 					$data[] = $res;
 			}
-
+				
 		return $data;
 	}
 
-	function DBUpdate($table, $data, $where = null, $insertID = false){
-
-		foreach($data as $key => $value)
-			$fields[] = "{$key} = '{$value}'";
-
-		$fields = implode(', ', $fields);
-
-		$where = ($where) ? " WHERE {$where}" : null;
-
-		$query = "UPDATE $table SET {$fields}{$where}";
-		
-		return DBExecute($query, $insertID);
-}
-
-	function DBDelete($table, $where = null){
-
-	 	$where = ($where) ? " WHERE {$where}" : null;
-
-	 	$query = "DELETE FROM {$table}{$where}";
-
-		return DBExecute($query);
 
 
-	 }
+
+
+
+
+
+
+
+
+
 
 
 ?>
